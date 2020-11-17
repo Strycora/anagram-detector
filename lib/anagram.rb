95e5d7525676eff91dd721e76fa624cbea31fd6d
Vikram Chatterjee <strycora@gmail.com>
2020-11-17 11:55:47 -0700

    commit tree 03a34c4830ccbb1bc19595cb5e051df891db27f7

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :keyword
+    def initialize(keyword)
+        @keyword = keyword
+    end
+    
+    def match(arr)
+        #select allows the enumerator to return multiple anagrams
+        arr.select do |word|
+            #splits the keyword and each word in arr into an array of letters, and 
+            #resorts the letters to see if a word in arr matches the letters in keyword 
+           keyword.split("").sort == word.split("").sort 
+        end
+
+    end
+
+end
